4275973cd430ec1365769328c1591a8915c70681
cagriTasbilek <tasbilekcagri@gmail.com>
2024-11-06 01:09:44 +0300

    AudioSysytem added

---
+++ CardMatching/Assets/Scripts/ScriptableObjects/AudioLibrarySO.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "AudioLibrary", menuName = "Audio/Audio Library")]
+public class AudioLibrarySO : ScriptableObject
+{
+    [Header("Game Sounds")]
+    public AudioEventSO cardFlip;
+    public AudioEventSO cardMatch;
+    public AudioEventSO cardMismatch;
+    public AudioEventSO gameOver;
+    public AudioEventSO gameStart;
+}
